0ad2cc0dacd88dd1875e94e08a233fe9a62df20a
Pedro Zanon <p.hzanon123@gmail.com>
2020-11-29 20:50:59 -0300

    create cursoRepository

--- src/main/java/br/com/pedrozanon/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/pedrozanon/forum/repository/TopicoRepository.java
@@ -3,6 +3,9 @@ package br.com.pedrozanon.forum.repository;
 import br.com.pedrozanon.forum.modelo.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
+    List<Topico> findByCursoNome(String nomeCurso);
 }
