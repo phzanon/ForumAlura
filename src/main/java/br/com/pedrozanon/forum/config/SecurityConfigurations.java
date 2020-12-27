e86a2d2c351fce4d11f3b0f262563c6422d695ab
Pedro Zanon <p.hzanon123@gmail.com>
2020-12-27 17:14:09 -0300

    ajustes

--- src/main/java/br/com/pedrozanon/forum/config/SecurityConfigurations.java
+++ src/main/java/br/com/pedrozanon/forum/config/SecurityConfigurations.java
@@ -52,6 +52,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .antMatchers(HttpMethod.GET,"/actuator/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -62,6 +63,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     /** Configurações de recursos estáticos -> js, css, imagens, etc. **/
     @Override
     public void configure(WebSecurity web) throws Exception {
-
+        web.ignoring()
+                .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**","swagger-resources/**");
     }
 }
